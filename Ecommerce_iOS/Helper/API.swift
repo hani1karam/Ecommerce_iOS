87c2ba967ee5e766d9630bb0aaf57ca2c91a10c5
hani1karam <hanykaram63@gmail.com>
2020-06-23 20:12:08 +0200

    IOS

---
+++ Ecommerce_iOS/Helper/API.swift
@@ -0,0 +1,21 @@
+//
+//  API.swift
+//  Ecommerce_iOS
+//
+//  Created by Hany Karam on 6/22/20.
+//  Copyright © 2020 Hany Karam. All rights reserved.
+//
+
+import Foundation
+let baseurl = "https://student.valuxapps.com/api/"
+let user = baseurl + "products/"
+let login = baseurl + "login"
+let register = baseurl + "register"
+let home = baseurl + "home"
+let fav = baseurl + "favorites"
+let carts = baseurl + "carts"
+let search = user + "search"
+let update = baseurl + "update-profile"
+let addresses = baseurl + "addresses"
+
+ 
